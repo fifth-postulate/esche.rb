261b56ae8dee670772cb1056fe9f100260ef3fbb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-03-29 20:43:45 +0200

    Flesh out vector function

--- src/lib/vector.rb
+++ src/lib/vector.rb
@@ -7,3 +7,15 @@ end
 def neg(u)
   Vector::new(-u.x, -u.y)
 end
+
+def sub(u, v)
+  add(u, neg(v))
+end
+
+def scale(factor, u)
+  Vector::new(factor * u.x, factor * u.y)
+end
+
+def length(u)
+  Math.sqrt(u.x * u.x + u.y * u.y)
+end
--- src/test/vector_test.rb
+++ src/test/vector_test.rb
@@ -21,4 +21,31 @@ class TestVector < Test::Unit::TestCase
     assert_equal(-1, w.x)
     assert_equal(-2, w.y)
   end
+
+  def test_subtraction
+    u = Vector.new(1, 0)
+    v = Vector.new(0, 2)
+
+    w = sub u, v;
+
+    assert_equal(1, w.x)
+    assert_equal(-2, w.y)
+  end
+
+  def test_scalar_multiplication
+    u = Vector.new(1, 2)
+
+    w = scale 2, u;
+
+    assert_equal(2, w.x)
+    assert_equal(4, w.y)
+  end
+
+  def test_length
+    u = Vector.new(3, 4)
+
+    l = length u
+
+    assert_equal(5.0, l)
+  end
 end
