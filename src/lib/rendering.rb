3e0bc8a218b0217c360110d7623393fbe42402bc
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-04-04 10:45:13 +0200

    Create a rudimentary to_svg_element function

--- src/lib/rendering.rb
+++ src/lib/rendering.rb
@@ -1,8 +1,23 @@
+require_relative "mirror"
+
+def to_svg_element(style, shape)
+  return <<ENDPART
+  <circle cx="10" cy="10" r="10">
+ENDPART
+end
+
 def to_svg(bounds, rendering)
 	w = bounds[0]
 	h = bounds[1]
+  mirror = lambda { |v| mirror_vector(h, v) }
+  svgs = rendering.map { |render|
+    shape = render[0]
+    style = render[1]
+    to_svg_element style, mirror_shape(mirror, shape)
+  }
 	return <<ENDSVG
 <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 #{w} #{h}">
+#{svgs.join("")}
 </svg>
 ENDSVG
 end
