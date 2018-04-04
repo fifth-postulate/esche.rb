54e3fd7bfaef8b275f66325bb7d87c1613c93b43
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-04-04 13:39:05 +0200

    Render polygons

--- src/lib/rendering.rb
+++ src/lib/rendering.rb
@@ -1,9 +1,42 @@
 require_relative "mirror"
 
+def to_polygon_element(style, vectors)
+  coordinates = vectors.map { |v| "#{v.x},#{v.y}" }
+  return <<TOPOLYGON
+  <polygon stroke="black" stroke-width="#{style.stroke_width}" fill="none" points="#{coordinates.join(" ")}" />
+TOPOLYGON
+end
+
 def to_svg_element(style, shape)
-  return <<ENDPART
-  <circle cx="10" cy="10" r="10">
+  if shape.instance_of?(Shape::Polygon)
+    to_polygon_element(style, shape.points)
+  # elsif shape.instance_of?(Shape::Polygon)
+  #   Shape::Polygon::new(shape.points.map { |p| m.(p) })
+  # elsif shape.instance_of?(Shape::PolyLine)
+  #   Shape::PolyLine::new(shape.pts.map { |p| m.(p) })
+  # elsif shape.instance_of?(Shape::Curve)
+  #   Shape::Curve::new(
+  #     m.(shape.point1),
+  #     m.(shape.point2),
+  #     m.(shape.point3),
+  #     m.(shape.point4)
+  #   )
+  # elsif shape.instance_of?(Shape::Path)
+  #   Shape::Path::new(
+  #     m.(shape.start),
+  #     shape.beziers.map { |p|
+  #       Shape::Proto::BezierShape::new(
+  #         m.(p.controlPoint1),
+  #         m.(p.controlPoint2),
+  #         m.(p.endPoint),
+  #       )
+  #     }
+  #   )
+  else
+    return <<ENDPART
+  <text></text>
 ENDPART
+  end
 end
 
 def to_svg(bounds, rendering)
