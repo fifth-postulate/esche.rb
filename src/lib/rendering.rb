a314834e04d2aa51f164f2800383b2587bc677c5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-04-12 10:55:25 +0200

    Add arrow heads to vectors

--- src/lib/rendering.rb
+++ src/lib/rendering.rb
@@ -75,10 +75,21 @@ def show_box(bounds, box)
   #Box::new(mirror_vector(h, box.a), mirror_vector(h, box.b), mirror_vector(h, box.c))
   return <<ENDBOX
 <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 #{w} #{h}">
+  <defs>
+    <marker id="red-triangle" viewBox="0 0 10 10" refX="10" refY="5" markerWidth="6" markerHeight="6" orient="auto">
+      <path d="M 0 0 L 10 5 L 0 10 z" fill="red" />
+    </marker>
+    <marker id="orange-triangle" viewBox="0 0 10 10" refX="10" refY="5" markerWidth="6" markerHeight="6" orient="auto">
+      <path d="M 0 0 L 10 5 L 0 10 z" fill="orange" />
+        <marker id="purple-triangle" viewBox="0 0 10 10" refX="10" refY="5" markerWidth="6" markerHeight="6" orient="auto">
+      <path d="M 0 0 L 10 5 L 0 10 z" fill="purple" />
+    </marker>
+</marker>
+  </defs>
   <g transform="scale(1,-1), translate(0, -#{h})">
-    <line x1="0" y1="0" x2="#{b.a.x}" y2="#{b.a.y}" stroke="red" />
-    <line x1="#{b.a.x}" y1="#{b.a.y}" x2="#{b.b.x + b.a.x}" y2="#{b.b.y + b.a.y}" stroke="orange" />
-    <line x1="#{b.a.x}" y1="#{b.a.y}" x2="#{b.c.x + b.a.x}" y2="#{b.c.y + b.a.y}" stroke="purple" />
+    <line x1="0" y1="0" x2="#{b.a.x}" y2="#{b.a.y}" stroke="red" marker-end="url(#red-triangle)" />
+    <line x1="#{b.a.x}" y1="#{b.a.y}" x2="#{b.b.x + b.a.x}" y2="#{b.b.y + b.a.y}" stroke="orange" marker-end="url(#orange-triangle)"  />
+    <line x1="#{b.a.x}" y1="#{b.a.y}" x2="#{b.c.x + b.a.x}" y2="#{b.c.y + b.a.y}" stroke="purple" marker-end="url(#purple-triangle)"  />
   </g>
 </svg>
 ENDBOX
